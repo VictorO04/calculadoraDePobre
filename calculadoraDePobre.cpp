eda4dcd99865c9456ad7e53340b50491ac0064af
Victor Ferreira <victorfoliveira0407@gmail.com>
2025-06-07 00:59:17 -0300

    Menu de operações

---
+++ calculadoraDePobre.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int operacao;
+
+    cout << "       ----- Seja bem vindo -----" << endl;
+    cout << "" << endl;
+    cout << "         Operacoes matematicas" << endl;
+    cout << "________________________________________ " << endl;
+    cout << "| 1 - Adicao             2 - subtracao |" << endl;
+    cout << "| 3 - Multiplicacao      4 - Divisao   |" << endl;
+    cout << "" << endl;
+    cout << "Digite o numero da operacao matematica que deseja realizar: ";
+    cin >> operacao;
+}
